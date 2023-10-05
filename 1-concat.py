8278d12c719414c1b2476c1750536e3e1b8914f6
elcis2614 <elisereflorbusole@gmail.com>
2023-10-05 23:53:14 +0000

    concatenating

---
+++ 1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+  annotated function with string parameters
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+       takes in two string and return a concatenated one
+    """
+    return srt1 + str2
