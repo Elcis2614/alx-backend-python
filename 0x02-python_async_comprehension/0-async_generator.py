f8094936fccfeb36adf297b9ca92b64510578e85
elcis2614 <elisereflorbusole@gmail.com>
2023-10-12 00:23:50 +0000

    Async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pythn3
+"""
+   async_generator : a coroutine that takes no argument
+   and uses random module to generate integers
+"""
+
+import asyncio
+import random
+import time
+
+
+async def async_generator() -> None:
+    """
+       It loops 10 times, each time asynchronously wait 1 second,
+       then yield a random number between 0 and 10
+    """
+    random.seed(220)
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield 10 * random.random()
