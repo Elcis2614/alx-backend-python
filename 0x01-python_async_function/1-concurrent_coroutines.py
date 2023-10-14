3cdb22b0758bbc416c8e0d2cf54bb4a12951e01c
elcis2614 <elisereflorbusole@gmail.com>
2023-10-14 02:15:32 +0000

    multiple coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,38 @@
+#!/usr/bin/even python3
+"""
+   Function wait_n executes multiple coroutines
+   by importing wait_random ( in 0-basic_async_syntax.py)
+"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def insertElement(element: float, lst: List[float]) -> None:
+    """
+       Insert a element at on the corresponding posision in
+       a sorted list
+    """
+    for i in range(len(lst)):
+        if element < lst[i]:
+            lst.insert(i, element)
+            break
+    else:
+        lst.append(element)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+       takes in 2 int arguments (in this order): n and max_delay.
+       Spawns wait_random n times with the specified max_delay
+       Returns the list of all the delays (float values) in ascending order
+    """
+    lst = list()
+    for i in range(n):
+        random = await asyncio.gather(wait_random(max_delay))
+        insertElement(random[0], lst)
+    return lst
